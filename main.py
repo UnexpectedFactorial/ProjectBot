5edf91ffbd7e0ccf493c1644d6e8066fb423324a
cr0ng <chengkevin34@gmail.com>
2023-02-26 04:04:52 +0000

    Removed urlib dependency for wikiscrape function. Reimplemented it with Python's Requests library for better functionality

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from PyDictionary import PyDictionary
 from discord.ext import commands
 
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
 
 token = os.environ['TOKEN']  #discord bot token
 
@@ -17,7 +16,7 @@ logger = settings.logging.getLogger("bot")
 
 def run():
   intents = discord.Intents.default()
-  intents.message_content = True
+  intents.all = True
 
   bot = commands.Bot(command_prefix="#", intents=intents)
 
@@ -32,6 +31,11 @@ def run():
   async def ping(ctx):
     await ctx.send("I am Alive")
 
+  @bot.event
+  async def on_member_join(member):
+    defaultrole = discord.utils.get(member.guild.roles, id="default")
+    await member.add_roles(defaultrole)
+
   @bot.command()
   async def repeat(ctx, *, word):
     print(word)
@@ -40,15 +44,21 @@ def run():
   @bot.command()
   async def wikiscrape(ctx, *, query):
     print(query)
-    page = urlopen(f"https://en.wikipedia.org/wiki/{query}")
+    page = requests.get(f"https://en.wikipedia.org/wiki/{query}")
     print(page)
-    html = page.read().decode("utf-8", "ignore")
-    site = BeautifulSoup(html, "html.parser")
+    site = BeautifulSoup(page.content, "html.parser")
 
     await ctx.send(f"**We are now scraping from:** {site.title.string}")
+    if page.status_code == 200:
+      if site.find_all("div", {"class": "mw-body-content mw-content-ltr"}):
+        for div in site.find_all('div', class_='mw-body-content mw-content-ltr'):
+          await ctx.send(site.find_all('p')[1].text)
+          
+    elif page.status_code == 404:
+      await ctx.send("Sorry, we couldn't find any response. Check your spelling?")
 
-    for div in site.find_all('div', class_='mw-body-content mw-content-ltr'):
-      await ctx.send(site.find_all('p')[1].text)
+    else:
+      await ctx.send("Connection error! Please try again later.")
 
   @bot.command()
   async def dog(ctx):
@@ -77,14 +87,15 @@ def run():
   async def google(ctx, *, arg):
     arg = arg.replace(' ', '_').replace('  ', '_')
     await ctx.send(f"https://www.google.com/search?q={arg}")
-  
+
   @bot.command()
   async def define(ctx, *, arg):
     dictionary = PyDictionary()
     result = dictionary.meaning(arg)
     await ctx.send(f"{result}")
-  
+
   bot.run(token, root_logger=True)
+
 
 if __name__ == "__main__":
   run()
